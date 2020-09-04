cec22c9a4378f5b6018c1e5d7115cf08a3c491d6
kolyaj <kolyaj@yandex.ru>
2020-09-04 18:02:09 +0300

    https://kolyaj.name/all/dresscodejs-todo-part02/

--- js-libs/Todo/App.js
+++ js-libs/Todo/App.js
@@ -1,7 +1,42 @@
+//#layer css
+Botex.CSS.add({
+    '.$$': {
+        'font': '14px arial',
+
+        'height': '100%'
+    },
+    '.$$__content': {
+        'height': '100%',
+
+        'display': 'flex'
+    },
+    '.$$__checkboxes': {
+        'width': '30px',
+
+        'background': '#ccc'
+    },
+    '.$$__text': {
+        'font': 'inherit',
+
+        'padding': '10px',
+
+        'flex-grow': '2',
+        'line-height': '25px',
+
+        'border': '0 none',
+        'resize': 'none'
+    }
+});
+//#endlayer
+
 Todo.App = Bricks.inherit(Botex.Widget, {
     _render: function() {
         return {
-            content: 'ToDo List'
+            className: '$$',
+            content: Botex.zen('.$$__content', [
+                Botex.zen('.$$__checkboxes'),
+                Botex.zen('textarea.$$__text')
+            ])
         };
     }
 });
