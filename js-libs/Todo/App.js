72b4bcdc16c76bd757fe5aa6839042cf11ab7f4e
kolyaj <kolyaj@yandex.ru>
2020-10-02 17:23:36 +0300

    https://kolyaj.name/all/dresscodejs-todo-part03/

--- js-libs/Todo/App.js
+++ js-libs/Todo/App.js
@@ -3,40 +3,85 @@ Botex.CSS.add({
     '.$$': {
         'font': '14px arial',
 
-        'height': '100%'
-    },
-    '.$$__content': {
-        'height': '100%',
-
-        'display': 'flex'
-    },
-    '.$$__checkboxes': {
-        'width': '30px',
+        'min-height': '100%',
+        'display': 'flex',
 
         'background': '#ccc'
     },
-    '.$$__text': {
-        'font': 'inherit',
+    '.$$__checkboxes': {
+        'position': 'relative',
 
+        'width': '30px'
+    },
+    '.$$__checkbox': {
+        'position': 'absolute',
+        'left': '0',
+
+        'width': '20px',
+        'height': '20px',
+        'padding': '5px'
+    },
+    '.$$__text': {
         'padding': '10px',
 
         'flex-grow': '2',
         'line-height': '25px',
 
-        'border': '0 none',
-        'resize': 'none'
+        'background': '#fff'
     }
 });
 //#endlayer
 
 Todo.App = Bricks.inherit(Botex.Widget, {
+    constructor: function() {
+        Todo.App.superclass.constructor.apply(this, arguments);
+        this._lines = new Botex.MutableArray();
+        this._on('$$__text', 'input', this.$$_onInput);
+    },
+
+    mount: function() {
+        Todo.App.superclass.mount.apply(this, arguments);
+        this._checkLines();
+    },
+
     _render: function() {
         return {
             className: '$$',
-            content: Botex.zen('.$$__content', [
-                Botex.zen('.$$__checkboxes'),
-                Botex.zen('textarea.$$__text')
-            ])
+            content: [
+                Botex.zen('.$$__checkboxes', this._lines.transform(function(line) {
+                    return Botex.if(line.hasText, function() {
+                        return Botex.zen('input.$$__checkbox[type=checkbox]', {
+                            style: {
+                                'top': line.offset
+                            }
+                        });
+                    })
+                }, this)),
+                Botex.zen('.$$__text[contenteditable=true]', Botex.zen('div', Botex.zen('br')))
+            ]
         };
+    },
+
+    _checkLines: function() {
+        [].slice.call(this._getEls('$$__text')[0].childNodes, 0).forEach(function(el) {
+            if (el.$$_meta) {
+                el.$$_meta.hasText.setValue(el.innerText.trim().length > 0);
+                el.$$_meta.offset.setValue(el.offsetTop);
+            } else {
+                el.$$_meta = {
+                    width: el.offsetWidth,
+                    hasText: new Quantum.Quant(el.innerText.trim().length > 0),
+                    offset: new Quantum.Quant(el.offsetTop)
+                };
+                this._lines.push(el.$$_meta);
+            }
+        }, this);
+        this._lines.filter(function(line) {
+            return line.width > 0;
+        });
+    },
+
+    $$_onInput: function() {
+        this._checkLines();
     }
 });
